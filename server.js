17b20f4e85123ba88d4bbb933ac13ae17df5829d
Xavier Jayet-Griffon <xavier.jayet-griffon@etu.univ-grenoble-alpes.fr>
2024-07-14 16:14:48 +0200

    Création de repertoire avec id unique

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
 
-    //On spécifie l'entête pour le CORS
+    // On spécifie l'entête pour le CORS
     const allowOrigins = 'http://127.0.0.1:5500';
     if (req.headers.origin && allowOrigins.includes(req.headers.origin)) {
         res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
@@ -15,14 +15,14 @@ const server = http.createServer((req, res) => {
         res.setHeader('Access-Control-Allow-Origin', 'null');
     }
 
-    //On gère le cas où le navigateur fait un pré-contrôle avec OPTIONS
+    // On gère le cas où le navigateur fait un pré-contrôle avec OPTIONS
     if (req.method === 'OPTIONS') {
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, Authorization');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         return res.end();
     }
 
-    //Traitement lorsqu'on reçoit un POST
+    // Traitement lorsqu'on reçoit un POST
     if (req.method === 'POST' && req.url === '/') {
         let body = '';
 
@@ -31,17 +31,22 @@ const server = http.createServer((req, res) => {
         });
 
         req.on('end', () => {
-            console.log('test');
             try {
                 const data = JSON.parse(body);
                 const editors = data.editors;
-                console.log(editors);
-                console.log(data);
-                //Pour chaque éditeur
+
+                // Générer un nom de répertoire unique
+                var basePath = path.join(__dirname, Math.floor(Math.random() * 1000).toString());
+                while (fs.existsSync(basePath)) {
+                    basePath = path.join(__dirname, Math.floor(Math.random() * 1000).toString());
+                }
+                //Création du répertoire 
+                fs.mkdirSync(basePath);
+
+                // Pour chaque éditeur
                 editors.forEach(editor => {
                     let filePath;
-                    // Définir le chemin du fichier en fonction du langage de l'éditeur
-                    const basePath = path.join(__dirname, 'files'); // Base path set to './files/'
+
                     if (editor.language === 'html') {
                         filePath = path.join(basePath, `editor_${editor.id}.html`);
                     } else if (editor.language === 'css') {
@@ -52,23 +57,23 @@ const server = http.createServer((req, res) => {
                         filePath = path.join(basePath, `editor_${editor.id}.txt`);
                     }
 
-                    //Écrire le contenu dans le fichier
-                    // fs.writeFile(filePath, editor.value, err => {
-                    //     if (err) {
-                    //         console.error(`Erreur lors de la création du fichier ${filePath}:`, err);
-                    //         res.writeHead(500, { 'Content-Type': 'application/json' });
-                    //         res.end(JSON.stringify({ success: false, message: `Erreur pour le fichier ${filePath}` }));
-                    //         return;
-                    //     }
-                    // });
+                    // Écrire le contenu dans le fichier et gestion des cas d'erreur
+                    fs.writeFile(filePath, editor.value, err => {
+                        if (err) {
+                            console.error(`Erreur lors de la création du fichier ${filePath}:`, err);
+                            res.writeHead(500, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ success: false, message: `Erreur pour le fichier ${filePath}` }));
+                            return;
+                        }
+                    });
                 });
 
                 res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ success: true, message: 'Fichier créer avec succès' }));
+                res.end(JSON.stringify({ success: true, message: 'Fichiers créés avec succès' }));
             } catch (err) {
                 console.error('Erreur parsing JSON:', err);
                 res.writeHead(400, { 'Content-Type': 'application/json' });
-                res.end();
+                res.end(JSON.stringify({ success: false, message: 'Erreur parsing JSON' }));
             }
         });
     } else if (req.method === 'GET') { // GET 
@@ -78,11 +83,11 @@ const server = http.createServer((req, res) => {
             <style></style>
             </head>
             <body>pas bon
-                <script>
-                    alert("pas bon");
-                <\/script>
         </body></html>`);
         res.end();
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ success: false, message: 'Route non trouvée' }));
     }
 });
 
